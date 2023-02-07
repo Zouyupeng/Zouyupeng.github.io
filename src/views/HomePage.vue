d7fc77520a348c3b08e75842d9cdb3cf1ad1788d
Oceanknight <2827280689zou@gmail.com>
2023-02-07 21:41:51 +0800

    🚧删成空白项目，导入arco组件库

---
+++ src/views/HomePage.vue
@@ -0,0 +1,15 @@
+<script setup>
+const handleClickMini = () => {
+  AMessage.info('hello Oceanknight, click mini AButton!')
+};
+</script>
+<template>
+  <div>hello Oceanknight, This is toolsdog home page!</div>
+  <a-space>
+    <a-button type="primary" size="mini" @click="handleClickMini">Mini</a-button>
+    <a-button type="primary" size="small">Small</a-button>
+    <a-button type="primary">Medium</a-button>
+    <a-button type="primary" size="large">Large</a-button>
+  </a-space>
+</template>
+<style scoped lang="scss"></style>
