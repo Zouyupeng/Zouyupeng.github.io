4212dbc58954e7a84866cc628dea7b28ee5c946e
Oceanknight <2827280689zou@gmail.com>
2023-02-21 13:04:14 +0800

    🚧用v-bind把+1按钮的属性提一下

--- src/components/plusOneButton.vue
+++ src/components/plusOneButton.vue
@@ -1,22 +1,28 @@
 <template>
   <a-space>
-    <a-button
-      type="primary"
-      shape="round"
-      size="medium"
-      status="success"
-      @click="increment"
-    >
+    <a-button v-bind="attr" @click="increment">
       {{ count }}
     </a-button>
+    <a-button @click="changeRed"> 按钮变红 </a-button>
   </a-space>
 </template>
 
 <script setup>
 const count = ref(0)
 
+var attr = ref({
+  type: 'primary',
+  shape: 'round',
+  size: 'large',
+  status: 'success'
+})
+
 function increment() {
   count.value++
+}
+
+function changeRed() {
+  attr.value.status = 'danger'
 }
 </script>
 
