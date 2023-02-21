2ab393a31cb7ed2301cab322cfd725ba67cfaec0
Oceanknight <2827280689zou@gmail.com>
2023-02-21 11:31:43 +0800

    🚧跟文档把+1按钮改成ref的

--- src/components/plusOneButton.vue
+++ src/components/plusOneButton.vue
@@ -1,16 +1,22 @@
 <template>
   <a-space>
-    <a-button type="primary" @click="increment">{{ state.count }}</a-button>
+    <a-button
+      type="primary"
+      shape="round"
+      size="medium"
+      status="success"
+      @click="increment"
+    >
+      {{ count }}
+    </a-button>
   </a-space>
 </template>
 
 <script setup>
-import { reactive } from 'vue'
-
-const state = reactive({ count: 0 })
+const count = ref(0)
 
 function increment() {
-  state.count++
+  count.value++
 }
 </script>
 
